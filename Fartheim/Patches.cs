99b46e1bdcd0fb9a7332a01dcd6cc5f3a19f1f51
Pfhoenix <pfhoenix@gmail.com>
2021-04-28 20:16:41 -0400

    initial push

---
+++ Fartheim/Patches.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Fartheim
+{
+	public class Farter : MonoBehaviour
+	{
+		static string[] BoneRoots = new string[] { "Hip", "Hips", "l_hip" };
+		AudioSource AS;
+		Character Char;
+		bool Crouching;
+		float TimeSinceLastFart;
+		float FartTimerDelay;
+		ParticleSystem PS;
+		float SizeScalar = 1;
+
+		public void Awake()
+		{
+			Char = GetComponentInChildren<Character>();
+			AS = gameObject.AddComponent<AudioSource>();
+			AS.playOnAwake = false;
+			AS.maxDistance = Plugin.FartNoiseRange.Value;
+			AS.rolloffMode = AudioRolloffMode.Linear;
+			AS.spatialBlend = 1f;
+			AS.outputAudioMixerGroup = AudioMan.m_instance.m_ambientMixer;
+
+			Transform psparent = null;
+			if (Char)
+			{
+				if (!gameObject.name.StartsWith("Skeleton") && !gameObject.name.StartsWith("Blob"))
+				{
+					string bone = null;
+					SkinnedMeshRenderer smr = gameObject.GetComponentInChildren<SkinnedMeshRenderer>();
+					if (smr)
+					{
+						if (gameObject.name.StartsWith("Boar") || gameObject.name.StartsWith("Wolf")) bone = "Pelvis";
+						else if (gameObject.name.StartsWith("Deer")) bone = "L Thigh";
+						else if (gameObject.name.StartsWith("Dragon")) bone = "Hips.001";
+						else if (gameObject.name.StartsWith("Draugr")) bone = "LeftHip";
+						else if (gameObject.name.StartsWith("Eikthyr")) bone = "Bone.009";
+						else if (gameObject.name.StartsWith("Hatchling")) bone = "Spine1";
+						else if (gameObject.name.StartsWith("Surtling")) bone = "mixamorig:Hips";
+						else if (gameObject.name.StartsWith("Troll")) bone = "Spine0";
+						if (bone != null)
+						{
+							psparent = smr.bones.FirstOrDefault(t => t.name == bone);
+							if (!psparent) psparent = transform;
+						}
+						else
+						{
+							psparent = smr.bones.FirstOrDefault(t => BoneRoots.Contains(t.name));
+						}
+					}
+				}
+			}
+
+			if (!psparent) psparent = transform;
+			GameObject go = GameObject.Instantiate(Plugin.FartSystemPrefab, psparent);
+			PS = go.GetComponent<ParticleSystem>();
+
+			CapsuleCollider cc = gameObject.GetComponentInChildren<CapsuleCollider>();
+			if (cc)
+			{
+				double r = cc.radius;
+				double h = cc.height;
+
+				double v = 4.0 / 3.0 * Math.PI * Math.Pow(r, 3.0);
+				h -= 2 * r;
+				if (h > 0) v += Math.PI * Math.Pow(r, 2.0);
+
+				SizeScalar = (float)Math.Sqrt(v);
+			}
+			else
+			{
+				SphereCollider sc = gameObject.GetComponentInChildren<SphereCollider>();
+				if (sc)
+				{
+					double v = 4.0 / 3.0 * Math.PI * Math.Pow(sc.radius, 3.0);
+					SizeScalar = (float)Math.Sqrt(v);
+				}
+			}
+
+			if (SizeScalar != 1f)
+			{
+				var main = PS.main;
+				main.startSpeedMultiplier = SizeScalar;
+				main.startSizeMultiplier = SizeScalar;
+				var vol = PS.velocityOverLifetime;
+				vol.speedModifierMultiplier = SizeScalar;
+			}
+		}
+
+		void Fart()
+		{
+			List<AudioClip> acs = (Char && Char.InWater()) ? Plugin.WetFarts : Plugin.DryFarts;
+			int index;
+			if (Char && Char.IsPlayer() && Plugin.BunsOfSteel.Value) index = Mathf.Min(acs.Count - 1, (int)(TimeSinceLastFart / Plugin.MaxFartTime.Value * acs.Count));
+			else index = Mathf.Min(acs.Count - 1, (int)(UnityEngine.Random.value * acs.Count));
+			AudioClip ac = acs[index];
+			AS.pitch = (0.75f + UnityEngine.Random.value * 0.5f) / SizeScalar;
+			AS.PlayOneShot(ac, Mathf.Clamp(TimeSinceLastFart / Plugin.MaxFartTime.Value, 0.25f, 1f));
+			FartTimerDelay = ac.length + 1f + UnityEngine.Random.value;
+			TimeSinceLastFart = 0;
+
+			PS.transform.rotation = Quaternion.LookRotation(-transform.forward, transform.up);
+			var main = PS.main;
+			main.duration = ac.length;
+			PS.Play();
+		}
+
+		public void Update()
+		{
+			if (!Player.m_localPlayer) return;
+
+			if (!Char || !Char.IsPlayer() || !Plugin.BunsOfSteel.Value)
+			{
+				if (FartTimerDelay <= 0)
+				{
+					TimeSinceLastFart += Time.deltaTime;
+					float r = UnityEngine.Random.value * Plugin.MaxFartTime.Value;
+					if (TimeSinceLastFart >= r) Fart();
+					else FartTimerDelay += 0.5f + UnityEngine.Random.value;
+				}
+				else
+				{
+					FartTimerDelay -= Time.deltaTime;
+					if (TimeSinceLastFart > 0) TimeSinceLastFart += Time.deltaTime;
+				}
+			}
+			else TimeSinceLastFart += Time.deltaTime;
+
+			if (Char && Char.IsCrouching())
+			{
+				if (!Crouching)
+				{
+					// crouching squeezes one out
+					Crouching = true;
+					Fart();
+				}
+			}
+			else if (Crouching) Crouching = false;
+
+			if (PS)
+			{
+				var fol = PS.forceOverLifetime;
+				if (Char && (Char.InInterior() || (Char.IsPlayer() && (Char as Player).InShelter())))
+				{
+					fol.x = 0;
+					fol.z = 0;
+				}
+				else
+				{
+					Vector3 w = EnvMan.instance.GetWindForce();
+					fol.x = w.x * 3;
+					fol.z = w.z * 3;
+				}
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(Character))]
+	public class CharacterPatches
+	{
+		[HarmonyPostfix]
+		[HarmonyPatch("Awake")]
+		public static void AwakePostfix(Character __instance)
+		{
+			__instance.gameObject.AddComponent<Farter>();
+		}
+	}
+
+	[HarmonyPatch(typeof(RandomFlyingBird))]
+	public class RandomFlyingBirdPatches
+	{
+		[HarmonyPostfix]
+		[HarmonyPatch("Start")]
+		public static void StartPostfix(RandomFlyingBird __instance)
+		{
+			if (Plugin.ShouldBirdsFart.Value) __instance.gameObject.AddComponent<Farter>();
+		}
+	}
+}
---
+++ Fartheim/Plugin.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Fartheim
+{
+	/*
+	 * Todo:
+	 * 
+	 * what about farts catching/spreading fire?
+	 * 
+	 */
+
+	[BepInPlugin("pfhoenix.fartheim", Plugin.ModName, Plugin.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		public const string Version = "1.1";
+		public const string ModName = "Fartheim";
+		Harmony _Harmony;
+		public static ManualLogSource Log;
+
+		AssetBundle AB;
+		public static List<AudioClip> DryFarts = new List<AudioClip>();
+		public static List<AudioClip> WetFarts = new List<AudioClip>();
+		public static GameObject FartSystemPrefab;
+
+		public static ConfigEntry<float> MaxFartTime;
+		public static ConfigEntry<float> FartNoiseRange;
+		public static ConfigEntry<bool> BunsOfSteel;
+		public static ConfigEntry<bool> ShouldBirdsFart;
+
+		private void Awake()
+		{
+#if DEBUG
+			Log = Logger;
+#else
+			Log = new ManualLogSource(null);
+#endif
+
+			AB = AssetHelper.LoadAssetBundle("fartheim");
+			var farts = AB.LoadAllAssets<AudioClip>();
+			foreach (var f in farts)
+			{
+				if (f.name.Contains("water")) WetFarts.Add(f);
+				else DryFarts.Add(f);
+			}
+			if (DryFarts.Count > 0) DryFarts = DryFarts.OrderBy(f => f.length).ToList();
+			if (WetFarts.Count > 0) WetFarts = WetFarts.OrderBy(f => f.length).ToList();
+
+			FartSystemPrefab = AB.LoadAsset<GameObject>("Assets\\FartSystem.prefab");
+			AssetHelper.RegisterPrefab(FartSystemPrefab);
+
+			MaxFartTime = Config.Bind("Farts", "Maximum Fart Time", 30f);
+			FartNoiseRange = Config.Bind("Farts", "Fart Noise Range", 32f);
+			BunsOfSteel = Config.Bind("Farts", "Players Only Fart When Crouching", true);
+			ShouldBirdsFart = Config.Bind("Farts", "Should Birds Fart", true);
+
+			_Harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+		}
+
+		private void OnDestroy()
+		{
+			if (_Harmony != null) _Harmony.UnpatchSelf();
+		}
+	}
+}
